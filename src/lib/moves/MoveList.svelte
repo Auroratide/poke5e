0ae3e84088e6af346f73aa69b7e7575a19484339
Timothy Foster <tf.auroratide@gmail.com>
2022-03-06 12:46:09 -0600

    Extract movelist component

---
+++ src/lib/moves/MoveList.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+    import type { Move } from './types'
+    import SortableTable from '../design/SortableTable.svelte'
+
+    export let moves: Move[]
+
+    const byStringField = (field: (m: Move) => string) => (l: Move, r: Move) => field(l).localeCompare(field(r))
+    const byNumericField = (field: (m: Move) => number) => (l: Move, r: Move) => field(l) - field(r)
+    const byPowerField = byStringField(it => typeof it.power === 'string' ? it.power : it.power.join(','))
+</script>
+
+<SortableTable let:item items={moves} headers={[ {
+    key: 'name', name: 'Name', sort: byStringField(it => it.name),
+}, {
+    key: 'type', name: 'Type', sort: byStringField(it => it.type),
+}, {
+    key: 'power', name: 'Power', sort: byPowerField,
+}, {
+    key: 'pp', name: 'PP', sort: byNumericField(it => it.pp),
+} ]}>
+    <tr class="row" style:--skin-local-bg="var(--skin-{item.type}-bg)">
+        <td class="name"><a href="/moves/{item.id}">{item.name}</a></td>
+        <td>{item.type}</td>
+        <td>{item.power === 'none' ? 'none' : item.power.join(', ')}</td>
+        <td>{item.pp}</td>
+    </tr>
+</SortableTable>
+
+<style lang="scss">
+    .row {
+        position: relative;
+        filter: drop-shadow(var(--elev-cumulus));
+
+        td {
+            background-color: var(--skin-content);
+            color: var(--skin-content-text);
+        }
+
+        td.name {
+            background-color: var(--skin-bg);
+            color: var(--skin-bg-text);
+
+            a {
+                color: var(--skin-bg-text);
+                text-decoration: none;
+            }
+        }
+
+        td:first-child {
+            border-radius: 1em 0 0 1em;
+        }
+
+        td:last-child {
+            border-radius: 0 1em 1em 0;
+        }
+
+        a::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+        }
+
+        &:hover {
+            td {
+                background-color: var(--skin-bg);
+                color: var(--skin-bg-text);
+            }
+
+            td.name {
+                background-color: var(--skin-local-bg);
+            }
+        }
+    }
+</style>
--- src/routes/moves/__layout.svelte
+++ src/routes/moves/__layout.svelte
@@ -16,6 +16,7 @@
 <script lang="ts">
     import type { Move } from '$lib/moves/types'
     import SortableTable from '$lib/design/SortableTable.svelte'
+    import MoveList from '$lib/moves/MoveList.svelte'
     import Backdrop from '$lib/design/Backdrop.svelte'
 
     export let moves: Move[]
@@ -28,22 +29,7 @@
 <Backdrop />
 <div class="page">
     <nav class="table" aria-label="Move List">
-        <SortableTable let:item items={moves} headers={[ {
-            key: 'name', name: 'Name', sort: (l, r) => l.name.localeCompare(r.name),
-        }, {
-            key: 'type', name: 'Type', sort: (l, r) => l.type.localeCompare(r.type),
-        }, {
-            key: 'power', name: 'Power', sort: (l, r) => l.power.join(',').localeCompare(r.power.join(',')),
-        }, {
-            key: 'pp', name: 'PP', sort: (l, r) => l.pp - r.pp,
-        } ]}>
-            <tr class="row" style:--skin-local-bg="var(--skin-{item.type}-bg)">
-                <td class="name"><a href="/moves/{item.id}">{item.name}</a></td>
-                <td>{item.type}</td>
-                <td>{item.power === 'none' ? 'none' : item.power.join(', ')}</td>
-                <td>{item.pp}</td>
-            </tr>
-        </SortableTable>
+        <MoveList {moves} />
     </nav>
     <main>
         <slot></slot>
@@ -61,56 +47,6 @@
         & > * {
             flex: 0 0 min(37.5rem, 100%);
             padding: 0.5em;
-        }
-    }
-
-    .table {
-        .row {
-            position: relative;
-            filter: drop-shadow(var(--elev-cumulus));
-
-            td {
-                background-color: var(--skin-content);
-                color: var(--skin-content-text);
-            }
-
-            td.name {
-                background-color: var(--skin-bg);
-                color: var(--skin-bg-text);
-
-                a {
-                    color: var(--skin-bg-text);
-                    text-decoration: none;
-                }
-            }
-
-            td:first-child {
-                border-radius: 1em 0 0 1em;
-            }
-
-            td:last-child {
-                border-radius: 0 1em 1em 0;
-            }
-
-            a::before {
-                content: '';
-                position: absolute;
-                top: 0;
-                right: 0;
-                bottom: 0;
-                left: 0;
-            }
-
-            &:hover {
-                td {
-                    background-color: var(--skin-bg);
-                    color: var(--skin-bg-text);
-                }
-
-                td.name {
-                    background-color: var(--skin-local-bg);
-                }
-            }
         }
     }
 </style>
