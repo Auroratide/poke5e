830ab043570e223e5b427eeab869932a7fb70459
Timothy Foster <tf.auroratide@gmail.com>
2022-03-07 13:57:49 -0600

    Create attribute block to show pokemon stats nicely

---
+++ src/lib/dnd/AttributeBlock.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import { modifierForScore } from './attributes'
+
+    export let attributes: {
+        str: number,
+        dex: number,
+        con: number,
+        int: number,
+        wis: number,
+        cha: number,
+    }
+
+    const modifierAsString = (n: number) => n >= 0 ? `+${n}` : `${n}`
+</script>
+
+<dl>
+    <dt><abbr title="Strength">str</abbr></dt>
+    <dd>{attributes.str} ({modifierAsString(modifierForScore(attributes.str))})</dd>
+    <dt><abbr title="Dexterity">dex</dt>
+    <dd>{attributes.dex} ({modifierAsString(modifierForScore(attributes.dex))})</dd>
+    <dt><abbr title="Constitution">con</dt>
+    <dd>{attributes.con} ({modifierAsString(modifierForScore(attributes.con))})</dd>
+    <dt><abbr title="Intelligence">int</dt>
+    <dd>{attributes.int} ({modifierAsString(modifierForScore(attributes.int))})</dd>
+    <dt><abbr title="Wisdom">wis</dt>
+    <dd>{attributes.wis} ({modifierAsString(modifierForScore(attributes.wis))})</dd>
+    <dt><abbr title="Charisma">cha</dt>
+    <dd>{attributes.cha} ({modifierAsString(modifierForScore(attributes.cha))})</dd>
+</dl>
+
+<style lang="scss">
+    dl {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        grid-auto-flow: column;
+        font-size: var(--font-sz-venus);
+
+        dt {
+            font-weight: 700;
+            text-transform: uppercase;
+        }
+
+        dd {
+            margin: 0;
+        }
+    }
+</style>
---
+++ src/lib/dnd/attributes.ts
@@ -0,0 +1,1 @@
+export const modifierForScore = (score: number): number => Math.floor(score / 2) - 5
