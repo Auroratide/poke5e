a667f5c3034c408b2987344921a25f5e38ae6089
Timothy Foster <tf.auroratide@gmail.com>
2022-03-06 18:06:30 -0600

    Add varies and any to move power types

--- src/lib/moves/types.ts
+++ src/lib/moves/types.ts
@@ -2,7 +2,7 @@ import type { PokeType } from '../pokemon/types'
 import type { Attribute } from '../dnd/types'
 import type { BodyText } from '../rendering/types'
 
-export type Power = Attribute[] | 'none'
+export type Power = Attribute[] | 'none' | 'any' | 'varies'
 
 export type Move = {
     id: string,
