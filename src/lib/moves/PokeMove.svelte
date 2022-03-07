3c7f6e52cc18ee307450fa03462e4f71f64b8697
Timothy Foster <tf.auroratide@gmail.com>
2022-03-07 07:28:22 -0600

    Better appearance for pokemon moves

--- src/lib/moves/PokeMove.svelte
+++ src/lib/moves/PokeMove.svelte
@@ -7,24 +7,26 @@
     export let move: Move
 </script>
 
-<article class="poke-move">
+<article class="poke-move" style:--skin-local="var(--skin-{move.type}-bg">
     <header>
         <h1>{move.name}</h1>
         <TypeTag type={move.type}></TypeTag>
     </header>
-    <dl>
-        <dt>Move Power</dt>
-        <dd class="power">{powerAsString(move.power)}</dd>
-        <dt>Move Time</dt>
-        <dd>{move.time}</dd>
-        <dt><abbr title="Power Points">PP</abbr></dt>
-        <dd>{move.pp}</dd>
-        <dt>Duration</dt>
-        <dd class="duration">{move.duration}</dd>
-        <dt>Range</dt>
-        <dd class="range">{move.range}</dd>
-    </dl>
-    <section>
+    <section class="info">
+        <dl>
+            <dt>Move Power</dt>
+            <dd class="power">{powerAsString(move.power)}</dd>
+            <dt>Move Time</dt>
+            <dd>{move.time}</dd>
+            <dt><abbr title="Power Points">PP</abbr></dt>
+            <dd>{move.pp}</dd>
+            <dt>Duration</dt>
+            <dd class="duration">{move.duration}</dd>
+            <dt>Range</dt>
+            <dd class="range">{move.range}</dd>
+        </dl>
+    </section>
+    <section class="description">
         {@html renderHtml(move.description)}
         {#if move.higherLevels !== undefined}
             <p><strong>At Higher Levels: </strong>{move.higherLevels}</p>
@@ -34,15 +36,21 @@
 
 <style lang="scss">
     .poke-move {
-        background-color: #fff;
+        background-color: var(--skin-content);
+        color: var(--skin-content-text);
         border-radius: 2em;
-        padding: 1em;
-        box-shadow: 0 0.25em 0.75em rgba(0, 0, 0, 0.15);
+        box-shadow: var(--elev-cirrus);
+        overflow: hidden;
 
         header {
             display: flex;
             align-items: center;
             justify-content: space-between;
+
+            background-color: var(--skin-bg);
+            color: var(--skin-bg-text);
+            padding: 0.5em 1em;
+            margin-bottom: 0.5em;
 
             h1 {
                 font-weight: 700;
@@ -50,10 +58,15 @@
             }
         }
 
+        section {
+            padding: 0 1em;
+        }
+
         dl {
             display: grid;
             grid-template-columns: auto 1fr;
             column-gap: 0.5em;
+            font-size: var(--font-sz-venus);
 
             dt {
                 font-weight: 700;
