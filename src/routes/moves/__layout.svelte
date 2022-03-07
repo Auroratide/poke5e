5124c19a6aeaf0ac4e534430db0fa01a51efc88f
Timothy Foster <tf.auroratide@gmail.com>
2022-03-07 13:03:06 -0600

    Display both text and list on same screen on mobile

--- src/routes/moves/__layout.svelte
+++ src/routes/moves/__layout.svelte
@@ -38,17 +38,36 @@
 
 <style lang="scss">
     .page {
-        display: flex;
-        flex-wrap: wrap;
-        margin: auto;
-        justify-content: center;
-        max-width: 75rem;
         height: 100%;
+        max-width: calc(0.5 * var(--container-width));
+        margin: auto;
 
-        & > * {
-            flex: 0 1 37.5rem;
+        nav, main {
             padding: 0.5em;
+        }
+
+        nav {
+            height: 33%;
+        }
+
+        main {
+            height: 67%;
+        }
+    }
+
+    @media screen and (min-width: 75rem) {
+        .page {
+            display: flex;
+            flex-wrap: wrap;
+            margin: auto;
+            justify-content: center;
+            max-width: 75rem;
             height: 100%;
+
+            nav, main {
+                flex: 0 1 37.5rem;
+                height: 100%;
+            }
         }
     }
 </style>
