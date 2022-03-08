e98f01f1f994ede2905bdb195d835f8b353bc103
Timothy Foster <tf.auroratide@gmail.com>
2022-03-07 21:01:26 -0600

    Allow card to be scrollable in the case of overflow

--- src/lib/design/Card.svelte
+++ src/lib/design/Card.svelte
@@ -1,21 +1,50 @@
 <script lang="ts">
+    import { onMount, onDestroy } from 'svelte'
+
     export let title: string
+
+    // Height calculations because my brain isn't big enough to figure out a CSS-only solution
+    let container: HTMLElement
+    let scrollable: HTMLElement
+    const setHeight = () => {
+        // The 3em accounts for the header and padding
+        scrollable.style.height = `min(${scrollable.scrollHeight}px, calc(${container.clientHeight}px - 3em))`
+    }
+
+    onMount(() => {
+        setHeight()
+        window.addEventListener('resize', setHeight)
+    })
+
+    onDestroy(() => {
+        if (typeof window !== 'undefined')
+            window.removeEventListener('resize', setHeight)
+    })
 </script>
 
-<article>
-    <header>
-        <h1>{title}</h1>
-        <slot name="header-extra"></slot>
-    </header>
-    <slot></slot>
-</article>
+<div bind:this={container} class="container">
+    <article>
+        <header>
+            <h1>{title}</h1>
+            <slot name="header-extra"></slot>
+        </header>
+        <div bind:this={scrollable} class="scrollable">
+            <slot></slot>
+        </div>
+    </article>
+</div>
 
 <style lang="scss">
+    .container {
+        height: 100%;
+    }
+
     article {
         background-color: var(--skin-content);
         color: var(--skin-content-text);
         border-radius: 2em;
         box-shadow: var(--elev-cirrus);
+        max-height: 100%;
         overflow: hidden;
 
         header {
@@ -32,6 +61,10 @@
                 font-weight: 700;
                 font-size: var(--font-sz-neptune);
             }
+        }
+
+        .scrollable {
+            overflow: auto;
         }
 
         :global(section) {
