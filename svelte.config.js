8a2280afdad533f906a0c67999d6989655e76fbe
Timothy Foster <tf.auroratide@gmail.com>
2022-03-11 14:19:12 -0600

    No base path when developing locally

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -12,7 +14,7 @@ const config = {
 			default: true,
 		},
 		paths: {
-			base: '/poke5e'
+			base: dev ? '' : '/poke5e'
 		},
 		adapter: adapter()
 	}
