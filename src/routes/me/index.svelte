13955bcd5f28c7abc1b841c5a298b95d1f79ccf9
Timothy Foster <tf.auroratide@gmail.com>
2022-03-17 18:51:52 -0500

    Begin adding a route for people's pokemon

---
+++ src/routes/me/index.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Page from '$lib/design/Page.svelte'
+    import Pokeball from '$lib/design/icon/Pokeball.svelte'
+</script>
+
+<svelte:head>
+    <title>My Pokemon | Pokemon 5e Reference</title>
+</svelte:head>
+
+<Page theme="purple">
+    <Pokeball slot="icon" />
+    <nav slot="side" class="table" aria-label="Pokemon List">
+        <p>Something</p>
+    </nav>
+    <p>Something 2</p>
+</Page>
+
+<style lang="scss">
+    nav {
+        height: 100%;
+    }
+</style>
