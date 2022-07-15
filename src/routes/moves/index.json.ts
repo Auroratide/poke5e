6e0ab1c9fbad85e7d9f14f21050df4750e2cd643
Timothy Foster <tf.auroratide@gmail.com>
2022-07-15 16:34:07 -0500

    Include contest in list api

--- src/routes/moves/index.json.ts
+++ src/routes/moves/index.json.ts
@@ -1,9 +1,15 @@
 import type { RequestHandler } from '@sveltejs/kit'
 import data from '../../../static/data/moves.json'
+import contestData from '../../../static/data/contest.json'
 
 export const get: RequestHandler = async () => {
+    const moves = data.moves.map(it => ({
+        ...it,
+        contest: contestData.items.find(c => c.id === it.id)
+    }))
+
     return {
         status: 200,
-        body: data,
+        body: { moves },
     }
 }
