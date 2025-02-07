ef44db0ae21de7004ac6c3e0beb9b9b8fe11cdce
Auroratide <tf.auroratide@gmail.com>
2025-02-06 20:53:48 -0600

    Allow container to be half sized

--- src/lib/design/Container.svelte
+++ src/lib/design/Container.svelte
@@ -1,10 +1,16 @@
-<div class="container">
+<script lang="ts">
+	export let half = false
+</script>
+
+<div class="container" class:half>
 	<slot></slot>
 </div>
 
 <style>
 	.container {
-		max-width: var(--container-width);
+		max-inline-size: var(--container-width);
 		margin: auto;
+	} .container.half {
+		max-inline-size: var(--container-half);
 	}
 </style>
