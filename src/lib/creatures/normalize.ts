eff240d519d5ed971f697d25a82cafed5a7a8ef7
Auroratide <tf.auroratide@gmail.com>
2025-03-12 07:30:26 -0500

    Include a warning if an ability is missing so it is easier to fix

--- src/lib/creatures/normalize.ts
+++ src/lib/creatures/normalize.ts
@@ -11,6 +11,10 @@ export const abilities = (allAbilities: {
 	...pokemon,
 	abilities: pokemon.abilities.map(ability => {
 		const matchedAbility = allAbilities.find(it => ability.id === it.id)
+		if (matchedAbility == null) {
+			console.warn(`Missing ability: ${ability.id}`)
+		}
+
 		return {
 			id: ability.id,
 			name: matchedAbility.name,
