3490365fc9403b8f47a9090b23af6f1862573821
Auroratide <tf.auroratide@gmail.com>
2025-03-16 19:01:57 -0500

    WIP

---
+++ src/routes/reference/catching-pokemon/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Card from "$lib/design/Card.svelte"
+	import Title from "$lib/design/Title.svelte"
+</script>
+
+<Title value="Catching Pokemon" />
+<Card title="Catching Pokémon">
+	<section>
+		<p>Catching Pokémon is crucial to your success as a trainer. There are several rules to follow when catching Pokémon:</p>
+		<ul>
+			<li>A trainer can hold no more Pokémon than their Pokéslots allow. If a trainer's Pokéslots are full when a Pokémon is caught, the trainer must choose one Pokémon to send to their PC.</li>
+			<li>A Pokéball is destroyed on a failed catch attempt.</li>
+			<li>A caught Pokémon retains its level, all status effects, and current HP at the time of the catch.</li>
+			<li>It is impossible for a Trainer to catch a Pokémon whose level is greater than their current level.</li>
+			<li></li>
+		</ul>
+	</section>
+</Card>
