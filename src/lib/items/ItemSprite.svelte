381f7355c2c7bed737dfa803a5aaf6b9b606a658
Auroratide <tf.auroratide@gmail.com>
2025-03-25 18:08:17 -0500

    (#32) Create a script to generate a list of possible items, and adjust page formatting to match fields

---
+++ src/lib/items/ItemSprite.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Art from "$lib/design/Art.svelte"
+
+	export let src: string
+	export let alt: string
+</script>
+
+<div class="pixelated">
+	<Art {src} {alt} />
+</div>
+
+<style>
+	.pixelated {
+		image-rendering: crisp-edges;
+		image-rendering: pixelated;
+	}
+</style>
--- src/routes/items.json/+server.ts
+++ src/routes/items.json/+server.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from "./$types"
 import data from "../../../static/data/items.json"
 
 export const GET: RequestHandler = async () => {
-	const items = data.items
+	const items = data.items.filter((it) => it.description != null)
 
 	return new Response(JSON.stringify({ items }), { status: 200 })
 }
