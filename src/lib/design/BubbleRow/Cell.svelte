72f6f4f929ee009d4ceafd246a863277e33367c6
Auroratide <tf.auroratide@gmail.com>
2025-04-14 17:44:59 -0500

    (#111) Use 'cellVisibility' to smartly show/hide columns in a table

--- src/lib/design/BubbleRow/Cell.svelte
+++ src/lib/design/BubbleRow/Cell.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
 	export let primary = false
+	export let cellVisibility: {
+		largeScreenOnly: boolean,
+		lastVisibleCell: boolean,
+	}
 </script>
 
-<td role="cell" class:primary>
+<td role="cell" class:primary class:large-screen-only={cellVisibility.largeScreenOnly} class:last-visible-cell={cellVisibility.lastVisibleCell}>
 	<slot></slot>
 </td>
+
+<style>
+	@media screen and (max-width: 37.5rem) {
+		.large-screen-only {
+			display: none;
+		}
+
+		.last-visible-cell {
+			border-top-right-radius: 1em;
+			border-bottom-right-radius: 1em;
+		}
+	}
+</style>
--- src/lib/items/ItemList.svelte
+++ src/lib/items/ItemList.svelte
@@ -19,17 +19,17 @@
 <div class="search-field">
 	<SearchField id="filter-items" label="Search" bind:value={$filterValue} matched={filteredItems.length} max={items.length} />
 </div>
-<SortableTable let:item items={filteredItems} bind:currentSorter={$currentSorter} headers={[ {
+<SortableTable let:item let:cellVisibility items={filteredItems} bind:currentSorter={$currentSorter} headers={[ {
 	key: "name", name: "Name", ratio: 3, sort: byStringField(it => it.name),
 }, {
 	key: "type", name: "Type", ratio: 2, sort: byStringField(it => it.type),
 }, {
-	key: "cost", name: "Cost", ratio: 2, sort: byNumericField(it => it.cost ?? -1),
+	key: "cost", name: "Cost", ratio: 2, sort: byNumericField(it => it.cost ?? -1), largeScreenOnly: true,
 } ]}>
 	<BubbleRow.Row interactive mainBg="var(--skin-{item.type.replace(" ", "-")}-bg)">
-		<BubbleRow.Cell primary><a href="{Url.items(item.id)}">{item.name}</a></BubbleRow.Cell>
-		<BubbleRow.Cell><span class="cap">{item.type}</span></BubbleRow.Cell>
-		<BubbleRow.Cell>{item.cost != null ? formatMoney(item.cost) : "-"}</BubbleRow.Cell>
+		<BubbleRow.Cell cellVisibility={cellVisibility[0]} primary><a href="{Url.items(item.id)}">{item.name}</a></BubbleRow.Cell>
+		<BubbleRow.Cell cellVisibility={cellVisibility[1]}><span class="cap">{item.type}</span></BubbleRow.Cell>
+		<BubbleRow.Cell cellVisibility={cellVisibility[2]}>{item.cost != null ? formatMoney(item.cost) : "-"}</BubbleRow.Cell>
 	</BubbleRow.Row>
 </SortableTable>
 
