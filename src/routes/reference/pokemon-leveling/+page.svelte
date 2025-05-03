724e7b1c8ebc61ad2db7fb1a8985e62b249bf963
Auroratide <tf.auroratide@gmail.com>
2025-05-03 12:00:52 -0500

    (#138) Add variable ASI from original PHB to reference

--- src/routes/reference/pokemon-leveling/+page.svelte
+++ src/routes/reference/pokemon-leveling/+page.svelte
@@ -6,6 +6,7 @@
 	import LevelTableRow from "./LevelTableRow.svelte"
 	import ExpTableRow from "./ExpTableRow.svelte"
 	import ExperienceTool from "./ExperienceTool.svelte"
+	import { Url } from "$lib/url"
 </script>
 
 <Title value="Pokémon Leveling" />
@@ -119,7 +120,13 @@
 		<p>Pokémon cannot know more than 4 moves at one time. Every time your Pokémon levels up, you may replace any one move it knows with any of the moves listed on its stat block at its level or lower. At levels 2, 6, 10, 14, and 18, your Pokémon gains access to new moves.</p>
 		<p>After evolving, a Pokémon retains all the moves it currently knows, even if its known moves are no longer listed on the evolved form's stat block. However, when switching a move upon level up, it may not learn a move from its previous evolutionary forms; only moves from its current form may be learned.</p>
 		<h3>Ability Score Improvement</h3>
-		<p>Your Pokémon can increase one ability score of your choice by 2, or two ability scores of your choice by 1 (to a maximum of 20). They can also instead select a Feat for Pokémon.</p>
+		<p>Your Pokémon gains a number of points to allocate to any of its ability scores, to a maximum of 20. The number of points to allocate depends on the number of evolutionary stages in that Pokémon's line:</p>
+		<ul>
+			<li><strong>1 stage:</strong> 4 points</li>
+			<li><strong>2 stages:</strong> 3 points</li>
+			<li><strong>3 stages:</strong> 2 points</li>
+		</ul>
+		<p>Up to 2 of these points may be used to acquire a <a href="{Url.reference.feats()}">Feat</a> instead.</p>
 		<h3>Damage Increase</h3>
 		<p>The damage of each damaging move increases at levels 5, 10, and 17, as shown in the move description.</p>
 		<h3>Peak Power</h3>
