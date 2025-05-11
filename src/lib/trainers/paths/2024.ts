f476c4ef8345187ea53c7963f85c9ba8022ce2b3
Auroratide <tf.auroratide@gmail.com>
2025-05-11 14:07:50 -0300

    (#93) Add the rest of the trainer paths, with tweaks here and there

--- src/lib/trainers/paths/2024.ts
+++ src/lib/trainers/paths/2024.ts
@@ -12,12 +12,12 @@ export const TrainerPaths: TrainerPath[] = [ {
 			description: ["When you reach level 5, you gain a number of battle dice (d6) equal to 1 + your Dexterity modifier (minimum of 1). You may assign one of these to any of your Pokémon to be added to a single attack or damage roll, after the result of the initial roll. You replenish your pool of battle dice at each long rest."],
 		},
 		level9: {
-			name: "Rapid Switching",
-			description: ["When you reach level 9, your Pokémon can choose to take a single action or move on the turn they are switched. They still cannot use a bonus action or a reaction on that turn.", "Additionally, your battle dice become a d8."],
+			name: "Tactical Mastery",
+			description: ["Certain trainers choose to excel in one arena, utilizing it to its fullest potential. When you reach level 9, choose an ability score. Each of your Pokémon increases that ability score by one. You must choose the same ability score to increase for all of your Pokémon. This increase will also apply to all Pokémon you catch in the future.", "Additionally, your battle dice become a d8."],
 		},
 		level15: {
-			name: "Tactical Mastery",
-			description: ["Certain trainers choose to excel in one arena, utilizing it to its fullest potential. When you reach level 15, choose an ability score. Each of your Pokémon increases that ability score by one. You must choose the same ability score to increase for all of your Pokémon. This increase will also apply to all Pokémon you catch in the future.", "Additionally, your battle dice become a d10."],
+			name: "Rapid Switching",
+			description: ["When you reach level 15, your Pokémon can choose to take a single action or move on the turn they are switched (not both). They still cannot use a bonus action or a reaction on that turn.", "Additionally, your battle dice become a d10."],
 		},
 	},
 }, {
@@ -40,22 +40,22 @@ export const TrainerPaths: TrainerPath[] = [ {
 		},
 	},
 }, {
-	name: "Nurse",
+	name: "Poké Mentor",
 	features: {
 		level2: {
-			description: ["You have a nurturing touch and a skill for mentoring Pokémon to bring out the best in them. You gain proficiency in the Medicine skill. Every time you use an item or ability to heal a Pokémon, that Pokémon regains addtional hit points equal to your Wisdom modifier (minimum one)."],
+			description: ["You have a skill for mentoring Pokémon to bring out the best in them. Beginning at level 2, your TMs can be used twice before breaking."],
 		},
 		level5: {
-			name: "Pokéchef",
-			description: ["You excel at creating meals for your Pokémon, seemingly out of nothing. At level 5, you are frequently prepared with some sort of edible treat, granting 2d4+2 hit points when given. You can give the treat to a creature as a bonus action and other creatures can administer it as an action. You can use this feature a number of times equal to your proficiency modifier per long rest."],
+			name: "Retention",
+			description: ["The Extra Move feat now costs one ASI point instead of two at levels 4, 8, 12, 16, or 20. If any of your Pokémon already has this feature, they may gain one ASI at this time."],
 		},
 		level9: {
-			name: "Field Medic",
-			description: ["At 9th level, you can perform first aid in the heat of battle. As an action, you may make a Medicine Check DC 12 to cure an adjacent Pokémon of a non-volatile status effect.", "In addition, your Pokéchef treat now heals 3d10+6 hit points."],
+			name: "Cheerleader",
+			description: ["At level 9, once per short rest, you may use a bonus action to boost all allied Pokémon with inspiring words. Until your next turn, you may add your CHA modifier (minimum of 1) to all allied attack rolls OR damage rolls OR AC."],
 		},
 		level15: {
-			name: "Tip-top Shape",
-			description: ["When you reach level 15, your Pokémon have advantage on saving throws against acquiring negative status conditions.", "In addition, your Pokéchef treat now heals 4d12+10 hit points."],
+			name: "Master Teacher",
+			description: ["You have become an incredibly skilled teacher, and move learning is accelerated under your leadership. When you reach level 15, your Pokémon's moves may be learned and unlearned at each long rest instead of level up."],
 		},
 	},
 }, {
@@ -78,6 +78,127 @@ export const TrainerPaths: TrainerPath[] = [ {
 		},
 	},
 }, {
+	name: "Pokémon Collector",
+	features: {
+		level2: {
+			description: ["Your fascination with all the different types of Pokémon of the world drives your need to collect them all. Beginning at level 2, you now have expertise in Animal Handling, doubling your proficiency in this skill."],
+		},
+		level5: {
+			name: "Gotta Catch 'Em All",
+			description: ["At level 5, once per long rest, you may roll an Animal Handling check with advantage, even if the opponent is not suffering from a negative status effect."],
+		},
+		level9: {
+			name: "Disciplined Strikes",
+			description: ["At level 9, you have trained your Pokémon to hold back or unleash power when necessary. When damaging a Pokémon enough to cause it to faint, you can choose to bring it to 1 HP instead."],
+		},
+		level15: {
+			name: "Expert Tracker",
+			description: ["At level 15, you become an expert at tracking Pokémon. When you use your Pokémon Tracker class feature, you may roll a DC 11 Investigation or Nature check in order to encounter any single Pokémon in the list of wild Pokémon for the area. Additionally, you may now use the Pokémon class feature twice per long rest."],
+		},
+	},
+}, {
+	name: "Nurse",
+	features: {
+		level2: {
+			description: ["You have a nurturing touch and a skill for mentoring Pokémon to bring out the best in them. You gain proficiency in the Medicine skill. Every time you use an item or ability to heal a Pokémon, that Pokémon regains addtional hit points equal to your Wisdom modifier (minimum one)."],
+		},
+		level5: {
+			name: "Pokéchef",
+			description: ["You excel at creating meals for your Pokémon, seemingly out of nothing. At level 5, you are frequently prepared with some sort of edible treat, granting 2d4+2 hit points when given. You can give the treat to a creature as a bonus action and other creatures can administer it as an action. You can use this feature a number of times equal to your proficiency modifier per long rest."],
+		},
+		level9: {
+			name: "Field Medic",
+			description: ["At 9th level, you can perform first aid in the heat of battle. As an action, you may make a Medicine Check DC 12 to cure an adjacent Pokémon of a non-volatile status effect.", "In addition, your Pokéchef treat now heals 3d10+6 hit points."],
+		},
+		level15: {
+			name: "Tip-top Shape",
+			description: ["When you reach level 15, your Pokémon have advantage on saving throws against acquiring negative status conditions.", "In addition, your Pokéchef treat now heals 4d12+10 hit points."],
+		},
+	},
+}, {
+	name: "Type Master",
+	features: {
+		level2: {
+			description: ["You feel drawn to the elements, focusing your skills into a particular type of Pokémon. Beginning at level 2, increase the STAB of any Pokémon that is the same type as your specialization by +1 at all levels. If you select multiple specializations at later levels, the bonus applies to the new type as well. If your Pokémon is dual-type and you are specialized in both types, the bonus is +2."],
+		},
+		level5: {
+			name: "Drawing Power",
+			description: ["Your Pokémon are trained to draw power from each other. When you reach level 5, Pokémon of the same type as your specialized types add +2 to their attack rolls."],
+		},
+		level9: {
+			name: "Storing Power",
+			description: ["Your skills within your specialized types give your Pokemon a great defensive power. When you reach level 9, all of your carried Pokémon within your specialized types have resistance to a choice of one of your specialization types, determined at the time of gaining this feature. This type may never change, so choose wisely. If a Pokémon was once vulnerable to that type, it now takes the regular amount of damage."],
+		},
+		level15: {
+			name: "Releasing Power",
+			description: ["Your Pokémon are masters of their own types, focusing their energy in every attack they make. When you reach level 15, for Pokémon of the same types you are specialized in, their STAB can be added to any damaging move of their choosing, even if it is a different type from their own."],
+		},
+	},
+}, {
+	name: "Commander",
+	features: {
+		level2: {
+			description: ["You rule over your Pokémon with an iron fist, demanding respect and forming an unbreakable bond with your team. When you choose this path at level 2, your starter’s Bond Level becomes +2. In addition, all of your with a positive Bond Level have 1 additional Bond Point."],
+		},
+		level5: {
+			name: "Follow Me",
+			description: ["You have a firm hand, but a trusting presence. When you reach level 5, any new Pokémon you catch get a +1 bonus to Bond Level (max Bond Level +2). In addition, your Pokémon's Bond Level cannot be decreased by losing a battle."],
+		},
+		level9: {
+			name: "Show Me What You've Got",
+			description: [
+				"Your confidence and leadership inspire your Pokémon to reach deep inside themselves for unknown power. At level 9, your Pokémon may spend a Bond Point to use any move they can learn at one tier higher than their current tier or below. Each Pokémon may only do this once per long rest.",
+				"For example, a Level 4 Bulbasaur can spend a Bond Point to use Sleep Power, since Sleep Power can be learned at one tier higher (level 6) than the Bulbasaur's current level.",
+			],
+		},
+		level15: {
+			name: "We're a Team",
+			description: ["Your commanding presence is felt by all ally Pokémon on the battlefield. When you reach level 15, you may use your reaction to speak a commanding phrase, allowing one of your Pokémon to spend one of its Bond Points to confer its benefit for an allied Pokémon, even if this allied Pokémon is not one of your own."],
+		},
+	},
+}, {
+	name: "Grunt",
+	features: {
+		level2: {
+			description: [
+				"Whether current or aspiring evil team member, your goal is to cast down all goody two shoe trainers and rise up in the ranks! Beginning at level 2, gain a pool of <strong>“Shadow Points”</strong> equal to your level. Your point pool increases with each level, and resets on each long rest.",
+				"At level 2, you gain access to the <strong>“Sabotage”</strong> ability. As a reaction, you may spend any number of points to decrease the total result of an attack roll including modifiers) that hits your Pokémon if it would cause the attack to miss. The natural attack roll is unaffected by this ability for purposes of move effects, and attacks with a natural roll of 20 may not be decreased at all."],
+		},
+		level5: {
+			name: "Dark Advantage",
+			description: ["You know the ins and outs of what it means to be a part of an evil team! At level 5, gain access to the <strong>“Dark Advantage”</strong> ability, allowing you to spend 3 Shadow Points to roll advantage on any skill check, attack roll or saving throw by you or your Pokémon."],
+		},
+		level9: {
+			name: "Sinister Dodge",
+			description: ["You have made it to the top ranks, commanding authority through your influence of your ruthless tactics. When you reach level 9, you gain the <strong>“Sinister Dodge”</strong> ability. As a reaction, you may spend 4 shadow points to add one level of resistance to a move that damages one of your Pokémon. (Vulnerable → Neutral, Neutral → Resistant, Resistant → Immune)"],
+		},
+		level15: {
+			name: "Nefarious Stagger",
+			description: ["No means are beneath you if it means attaining victory, so you and your Pokémon fully embrace the darkness within. At level 15, gain access to the <strong>“Nefarious Stagger”</strong> ability, allowing you to spend 2 Shadow Points whenever any of your Pokémon inflict the Flinched condition to inflict the Stunned condition instead."],
+		},
+	},
+}, {
+	name: "Tactician",
+	features: {
+		level2: {
+			description: [
+				"You have an eye for detail and a unique set of skills to use in battle. Beginning at level 2, you gain a pool of <strong>\"Tactical Points\"</strong> equal to your trainer level. Your point pool increases with each level, and resets after each long rest.",
+				"At level 2, when a Pokémon regains hit points from an item or move, you may increase the amount by 1d4 for each \"Tactical Point\" spent."],
+		},
+		level5: {
+			name: "Directed Strike",
+			description: ["You know precisely where to hit a Pokémon for the maximum amount of damage. At level 5 and higher, you can spend 2 Tactical Points to roll the damage for an attack twice, taking the higher result."],
+		},
+		level9: {
+			name: "Raise Your Defenses",
+			description: ["You are quickly able to sense an incoming blow to your Pokémon. At level 9 and higher, you can use a reaction to expend up to 5 of your Tactical Points to add to a Pokémon's AC or Saving Throw, if it would cause an attack to miss or the Pokémon to succeed its save."],
+		},
+		level15: {
+			name: "Not This Time",
+			description: ["At level 15, your Pokémon are trained to be unrelenting in battle, never holding back. After an opponent rolls a saving throw for one of your Pokémon's moves, you may increase the DC by up to 5 points if it would cause a failure, spending a Tactical Point for each increase."],
+		},
+	},
+}, {
 	name: "Ranger",
 	features: {
 		level2: {
@@ -93,7 +214,45 @@ export const TrainerPaths: TrainerPath[] = [ {
 		},
 		level15: {
 			name: "Poké Assist",
-			description: ["At level 15, your partner Pokémon boosts the power of your Capture Styler. When any of your Pokémon use a move that is the same type as either of your partner Pokémon's types, the move's attack roll is made with advantage. Additionally, you have advantage on making captures against Pokémon that share a type with your partner Pokémon."],
+			description: ["At level 15, your partner Pokémon boosts the power of your Capture Styler. When any of your Pokémon use a move that is the same type as either of your current partner Pokémon's types, the move's attack roll is made with advantage. Additionally, you have advantage on making captures against Pokémon that share a type with your partner Pokémon."],
+		},
+	},
+}, {
+	name: "Guru",
+	features: {
+		level2: {
+			description: ["You and your Pokémon are connected by way of more than just trainer and beast. Your past has trained you to be in complete control of your Mind, Body, and Spirit, and you transfer that aura to your Pokémon. When you choose this path at level 2, your Max SR you can control increases by 1."],
+		},
+		level5: {
+			name: "Mind",
+			description: ["Your presence in battle keeps your Pokémon at ease, so that they can fully focus on the task at hand without distraction. At level 5, all of your Pokémon are proficient in Wisdom saving throws and have advantage on the roll to avoid failing confusion."],
+		},
+		level9: {
+			name: "Body",
+			description: ["You have trained your Pokémon to focus deep within themselves to unleash their full potential. At level 9, all of your Pokémon have access to two of their abilities (if two are available). In addition, the Tireless feat now costs one ASI point instead of two at levels 4, 8, 12, 16, or 20. If any of your Pokémon already has this feature, they may gain one ASI at this time."],
+		},
+		level15: {
+			name: "Spirit",
+			description: ["Your connection to your Pokémon is masterful. Trainer and beast move together in perfect harmony. At level 15, you may control a Pokémon as if it were an extension of your body. At the beginning of your turn, you may activate this feature to add your WIS modifier to all attack OR damage rolls your Pokémon makes until the beginning of your next turn. The option of attack OR damage must be selected each time this is activated, and cannot change during the round. You may use this feature a number of times equal to 1 + your Wisdom modifier (minimum of 1), resetting this pool at each long rest."],
+		},
+	},
+}, {
+	name: "Pokémon Breeder",
+	features: {
+		level2: {
+			description: ["You have a fascination with Pokémon that has led you down the path of raising them carefully to create specific improvements in their species. Beginning at level 2, when you attempt to breed two Pokémon, you may add your WIS modifier to the d20 roll for a successful attempt."],
+		},
+		level5: {
+			name: "Tender Love and Care",
+			description: ["As a Breeder, you have studied Pokémon eggs and know just what they need to create the perfect environment for optimal growth, no matter the species. When you reach level 5, gain advantage on all rolls that reduce the incubation counter for Pokémon egg hatching times."],
+		},
+		level9: {
+			name: "Good Genes",
+			description: ["Your skill as a Breeder shines as people around begin to notice how much stronger your Pokémon seem to be than their typical counterparts. When you reach level 9, any Pokémon you hatch (or have hatched) from an egg gains 2 points to add to its abilities, or spend on a feat."],
+		},
+		level15: {
+			name: "Enhanced Diversity",
+			description: ["You are a master of your craft, able to unlock power from Pokémon never thought possible. When you reach level 15, for any future hatched Pokémon, it may inherit a single move from one of its parents that the parent knows, even if that move is not in the child Pokémon's moveset, as long as the parent's SR is less than 15."],
 		},
 	},
 } ]
--- src/routes/reference/introduction/+page.svelte
+++ src/routes/reference/introduction/+page.svelte
@@ -29,5 +29,96 @@
 			<li>Added multiclassing potential.</li>
 			<li>Simplified weather, so its effect is more subtle, and because the 2018 tables assumed only a specific kind of biome.</li>
 		</ul>
+		<p>Additionally, several trainer paths were tweaked:</p>
+		<ul>
+			<li>
+				<strong>Ace Trainer</strong>
+				<ul>
+					<li>Battle Dice now upgrade.</li>
+					<li>Max Potential → Tactical Mastery: More general to allow Ace Trainers of the INT, WIS, or CHA variety.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Hobbyist</strong>
+				<ul>
+					<li>Skill Dice now upgrade.</li>
+					<li>Many Faces → Generalist: Taking a different path's feature didn't always naturally work since paths usually build on themselves.</li>
+					<li>Skill Switch → Multitalented: Changed to emphasize the importance of bringing a well-rounded Pokémon team.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Poké Mentor</strong>
+				<ul>
+					<li>Pokéchef → Retention: Made Pokéchef a Nurse feature, and replaced it with something that focuses more on improving Pokémon movesets.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Researcher</strong>
+				<ul>
+					<li>Professor: Improves the Analyst feature to emphasize the researcher's data gathering personality rather than their battle prowess.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Pokémon Collector</strong>
+				<ul>
+					<li>Disciplined Strikes became a Level 9 feature, both so Expert Tracker could be the level 15 feature, and so it gets more use.</li>
+					<li>Catching Expert → Expert Tracker: Automatically encountering a Pokémon of your choice use to be available to all trainers with the Tracker class feature. Now, it is the collector's specialty.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Nurse</strong>
+				<ul>
+					<li>Pure Heart → Pokéchef: Nurse inherited Poké Mentor's chef ability to be the exclusive path with enhanced healing.</li>
+					<li>Healing Spirit → Field Medic: Gives the Nurse an additional way to keep their Pokémon healthy in combat.</li>
+					<li>Joy → Tip-top Shape: Changed to emphasize the Nurse's continual care of their Pokémon.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Type Master</strong>
+				<ul>
+					<li>No changes.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Commander</strong>
+				<ul>
+					<li>Generally updated to focus on their bond with their Pokémon, and to make more use of their Pokémon's Bond Points.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Grunt</strong>
+				<ul>
+					<li>Renamed their features to match the name of the abilities they gain.</li>
+					<li>Copy Meowth → Nefarious Stagger: More straightforward, and stunning is a bit more "evil" from a gameplay perspective.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Tactician</strong>
+				<ul>
+					<li>Raise your Defenses: Buffed to include saving throws, as it was otherwise equivalent to the Grunt's level 2 Sabotage ability.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Ranger</strong>
+				<ul>
+					<li>Overhauled both to more closely match Rangers in Pokémon lore and to account for the 2024 rules allowing control of up to two Pokémon.</li>
+					<li>Deep Connection → Capture Styler: Instead of being able to cast a literal spell, focus on the unique way Rangers calm wild Pokémon.</li>
+					<li>Strong Bond → Partners: Partner Pokémon are also a signature of Rangers in the lore.</li>
+					<li>Best Friends → Poké Assist: Poké Assist is a signature skill of Rangers in the lore.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Guru</strong>
+				<ul>
+					<li>Simplified the concept of SR-enhancement by directly buffing the Guru's controllable SR.</li>
+				</ul>
+			</li>
+			<li>
+				<strong>Breeder</strong>
+				<ul>
+					<li>Master of Traits → Enhanced Diversity: Sidestepping the morality of designer babies and replacing it with a way to spur more creative potential.</li>
+				</ul>
+			</li>
+		</ul>
 	</section>
 </ReferencePage>
