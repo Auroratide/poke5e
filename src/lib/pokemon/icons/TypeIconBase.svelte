df8554e59f226d903d7e7f80aaa62f7ebea49f88
Auroratide <tf.auroratide@gmail.com>
2025-05-13 10:04:10 -0300

    Add pokemon type icons

---
+++ src/lib/pokemon/icons/TypeIconBase.svelte
@@ -0,0 +1,17 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+	<slot></slot>
+</svg>
+
+<style>
+	svg {
+		display: inline-block;
+		overflow: visible;
+		inline-size: 1em;
+		block-size: 1em;
+	}
+
+	svg :global(path), svg :global(circle) {
+		fill: currentColor;
+		fill-rule: evenodd;
+	}
+</style>
