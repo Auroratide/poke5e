13c78ed804802dfa2c339b44b1e9d2794715b4a3
Auroratide <tf.auroratide@gmail.com>
2025-07-13 19:09:46 -0500

    Update evolution text to match the PHB

--- src/routes/reference/pokemon-leveling/2018/Evolution.svelte
+++ src/routes/reference/pokemon-leveling/2018/Evolution.svelte
@@ -1,10 +1,10 @@
 <ol>
-	<li>It keeps its current ability scores, but gains points to distribute towards any of those scores. No more than 4 of these points may be put into a single stat and no stat may be increased above 20. The number of points to distribute is provided on its stat block. These points cannot be used to acquire feats, unless specified otherwise.</li>
-	<li>It gains an HP bonus equal to double its level.</li>
-	<li>It acquires the hit dice of its evolved form for this level up and for future rolls.</li>
+	<li>It keeps its current ability scores, but gains a number of points to add to these scores as determined in its stat block. A Pokémon evolved in this way may not use evolution points to raise a single ability score higher than 4 points above the same score in their evolved form’s stat block, +/- applicable nature bonus, + any ASI points applied to that stat prior to evolution. (Maximum of 20 or higher score as stated in the evolved form’s stat block, before nature). With the exception of the Researcher training path feature, these points CANNOT be used for feats.</li>
+	<li>It gains an HP bonus of double its level.</li>
+	<li>It acquires the hit dice of its evolved form to increase HP this level and for future rolls.</li>
 	<li>It acquires the base AC of its evolved form, all new proficiencies, and vulnerabilities/resistances/immunities.</li>
 	<li>If it loses its current ability by evolving, it should exchange its current known ability for any one of its evolved form's non-hidden abilities.</li>
-	<li>It keeps the known moves it had at the time of evolution, but must learn all future moves from its new move list.</li>
+	<li> It keeps the known moves it had before evolution, but must learn all future moves from its new move list. For example, Pikachu evolving into Raichu at level 10 cannot learn any of Pikachu's level 10 moves.</li>
 	<li>If evolving at a level where it would gain ASI (4, 8, 12, 16, or 20), it adds those now.</li>
 </ol>
 <p>Evolution can be delayed at the player’s discretion, but once the decision has been made, the Pokémon cannot evolve until gaining an additional level.</p>
