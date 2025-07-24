b7dca444aa7f32ef59ce12adbcba194c7f7dd774
Auroratide <tf.auroratide@gmail.com>
2025-07-24 17:15:23 -0500

    Make types list more pretty

---
+++ src/lib/pokemon/types-2/InlineTypeName.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { PokeType } from "./PokemonType"
+	import { TypeIcon } from "./icons"
+
+	export let value: PokeType
+</script>
+
+<span class="inline-type-name" style:--color="var(--skin-{value}-bg)">
+	<span class="icon" aria-hidden="true">
+		<TypeIcon type={value} />
+	</span>
+	<span class="name">
+		{value}
+	</span>
+</span>
+
+<style>
+	.inline-type-name {
+		display: inline-flex;
+		flex-direction: row;
+		align-items: center;
+		color: var(--skin-bg-text);
+		text-transform: capitalize;
+		overflow: hidden;
+		border-radius: 0.25em;
+	}
+
+	.icon {
+		background: var(--color);
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		inline-size: 1.375em;
+		block-size: 1.375em;
+	} .icon :global(svg) {
+		font-size: 1.2em;
+	}
+
+	.name {
+		background: var(--skin-bg);
+		padding-inline: 0.375em;
+		padding-block: 0.0625em;
+	}
+</style>
