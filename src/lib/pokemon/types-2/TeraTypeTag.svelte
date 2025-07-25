10de7695c2da80bd21ea7e92f18d7fc85d8c3dba
Auroratide <tf.auroratide@gmail.com>
2025-07-24 20:02:26 -0500

    More accurate tera tag

--- src/lib/pokemon/types-2/TeraTypeTag.svelte
+++ src/lib/pokemon/types-2/TeraTypeTag.svelte
@@ -11,12 +11,38 @@
 <style>
    .tera-type-tag {
       --inset-amount: 0.25em;
+		--s1: 0.25em;
+		--s2: 0.11em;
       display: inline-block;
       background: var(--skin-local-bg);
       color: var(--skin-bg-text);
       text-transform: capitalize;
       box-shadow: 0 -0.667em 0 0em oklch(0% 0 0 / 0.2) inset;
-      padding: 0.0625em 0.5em;
-      clip-path: polygon(var(--inset-amount) 0%, calc(100% - var(--inset-amount)) 0%, 100% 50%, calc(100% - var(--inset-amount)) 100%, var(--inset-amount) 100%, 0% 50%);
+      padding: 0.0625em 0.625em;
+		clip-path: polygon(
+			calc(var(--inset-amount) + var(--s2)) var(--s1),
+			var(--inset-amount) 0%,
+			calc(var(--inset-amount) + var(--s1)) var(--s2),
+
+			calc(100% - var(--inset-amount) - var(--s1)) var(--s2),
+			calc(100% - var(--inset-amount)) 0%,
+			calc(100% - var(--inset-amount) - var(--s2)) var(--s1),
+
+			calc(100% - var(--s1)) calc(50% - var(--s2)),
+			100% 50%,
+			calc(100% - var(--s1)) calc(50% + var(--s2)),
+
+			calc(100% - var(--inset-amount) - var(--s2)) calc(100% - var(--s1)),
+			calc(100% - var(--inset-amount)) 100%,
+			calc(100% - var(--inset-amount) - var(--s1)) calc(100% - var(--s2)),
+
+			calc(var(--inset-amount) + var(--s1)) calc(100% - var(--s2)),
+			var(--inset-amount) 100%,
+			calc(var(--inset-amount) + var(--s2)) calc(100% - var(--s1)),
+
+			var(--s1) calc(50% + var(--s2)),
+			0% 50%,
+			var(--s1) calc(50% - var(--s2))
+		);
    }
 </style>
