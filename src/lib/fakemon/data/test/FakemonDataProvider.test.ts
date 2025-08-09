97013fd4f022a3cb03856f9aa289a6d979e69ec6
Auroratide <tf.auroratide@gmail.com>
2025-08-09 08:41:45 -0500

    Ok, enough ignoring these tests

--- src/lib/fakemon/data/test/FakemonDataProvider.test.ts
+++ src/lib/fakemon/data/test/FakemonDataProvider.test.ts
@@ -1,36 +1,34 @@
-import { test } from "vitest"
-// import { provider } from ".."
-// import { stubFakemon } from "./stubs"
-// import { FakemonPermissionError } from "../FakemonDataProvider"
+import { test, expect } from "vitest"
+import { provider } from ".."
+import { FakemonPermissionError } from "../FakemonDataProvider"
+import { stubFakemon } from "$lib/fakemon/test/stubs"
 
-test("placeholder", () => {})
+test("add, get, and update", async () => {
+	const draft = stubFakemon({
+		speciesName: "Droideon",
+	})
 
-// test("add, get, and update", async () => {
-// 	const draft = stubFakemon({
-// 		speciesName: "Droideon",
-// 	})
+	const added = await provider.add(draft.data)
+	const received = await provider.getByReadKey(added.data.readKey)
+	expect(received.data.speciesName).toEqual(draft.data.speciesName)
 
-// 	const added = await provider.add(draft.data)
-// 	const received = await provider.getByReadKey(added.data.readKey)
-// 	expect(received.data.speciesName).toEqual(draft.data.speciesName)
+	added.data.speciesName = "Aereon"
+	const didUpdate = await provider.update(added)
+	expect(didUpdate).toBe(true)
 
-// 	added.data.speciesName = "Aereon"
-// 	const didUpdate = await provider.update(added)
-// 	expect(didUpdate).toBe(true)
+	const afterUpdate = await provider.getByReadKey(added.data.readKey)
+	expect(afterUpdate.data.speciesName).toEqual("Aereon")
+})
 
-// 	const afterUpdate = await provider.getByReadKey(added.data.readKey)
-// 	expect(afterUpdate.data.speciesName).toEqual("Aereon")
-// })
+test("no permission to update", async () => {
+	const draft = stubFakemon({
+		speciesName: "Droideon",
+	})
 
-// test("no permission to update", async () => {
-// 	const draft = stubFakemon({
-// 		speciesName: "Droideon",
-// 	})
+	const added = await provider.add(draft.data)
+	const received = await provider.getByReadKey(added.data.readKey)
 
-// 	const added = await provider.add(draft.data)
-// 	const received = await provider.getByReadKey(added.data.readKey)
-
-// 	// result of get is not supposed to have a write key
-// 	received.data.speciesName = "Aereon"
-// 	await expect(provider.update(received)).rejects.toThrow(FakemonPermissionError)
-// })
+	// result of get is not supposed to have a write key
+	received.data.speciesName = "Aereon"
+	await expect(provider.update(received)).rejects.toThrow(FakemonPermissionError)
+})
