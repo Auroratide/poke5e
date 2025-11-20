30fc8ed8cdaf80cee6f0de0536380f6178ca252a
Auroratide <tf.auroratide@gmail.com>
2025-11-19 19:27:29 -0600

    (#227) More reliably load fakemon for generating async text

--- src/lib/rendering/ResolveAsyncText.svelte
+++ src/lib/rendering/ResolveAsyncText.svelte
@@ -4,28 +4,44 @@
 	* {{TYPE::ID}} -> render the thing's as a link
 -->
 <script lang="ts">
-	import { SpeciesIdentifier, SpeciesStore } from "$lib/creatures/species"
+	import { onMount, onDestroy } from "svelte"
+	import { PokemonSpecies, SpeciesIdentifier, SpeciesStore } from "$lib/creatures/species"
 	import LoaderInline from "$lib/design/LoaderInline.svelte"
 	import { moves } from "$lib/moves/store"
 	import { abilities } from "$lib/pokemon/store"
 	import { Url } from "$lib/url"
-	import { get } from "svelte/store"
+	import { type Unsubscriber } from "svelte/store"
 
-	async function getSpecies() {
-		return get(await SpeciesStore.completeList())
-	}
+	let loading = true
+	let species: PokemonSpecies[] = []
+	let unsubscribe: Unsubscriber = undefined
+
+	onMount(() => {
+		SpeciesStore.completeList().then((store) => {
+			unsubscribe = store.subscribe((s) => {
+				if (s != null && s.length > 0) {
+					loading = false
+					species = s
+				}
+			})
+		})
+	})
+
+	onDestroy(() => {
+		unsubscribe?.()
+	})
 	
 	export let value: string
 
 	const isFakemon = (id: string) => new SpeciesIdentifier(id).isFakemon()
 </script>
 
-{#await getSpecies()}
+{#if loading}
 	<LoaderInline />
-{:then species}
+{:else}
 	{@html value
 		.replaceAll(/{{pokemon:(:?)(.*?)}}/g, (_, link, id) => link !== "" ? `<a href="${isFakemon(id) ? Url.fakemon(new SpeciesIdentifier(id).toFakemonReadKey()) : Url.pokemon(id)}">${species.find((it) => it.id.data === id)?.data.name}</a>` : species.find((it) => it.id.data === id)?.data.name)
 		.replaceAll(/{{move:(:?)(.*?)}}/g, (_, link, id) => link !== "" ? `<a href="${Url.moves(id)}">${$moves.find((it) => it.id === id)?.name}</a>` : $moves.find((it) => it.id === id)?.name)
 		.replaceAll(/{{ability:(:?)(.*?)}}/g, (_, link, id) => link !== "" ? `<a href="${Url.reference.abilities()}#${id}">${$abilities.find((it) => it.id === id)?.name}</a>` : $abilities.find((it) => it.id === id)?.name)
 	}
-{/await}
+{/if}
