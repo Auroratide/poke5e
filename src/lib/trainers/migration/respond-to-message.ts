ebfe8fa793f3bca9e4395adf9152a5b0ef3f797e
Auroratide <tf.auroratide@gmail.com>
2025-12-20 12:21:18 -0600

    Fix compilation error

--- src/lib/trainers/migration/respond-to-message.ts
+++ src/lib/trainers/migration/respond-to-message.ts
@@ -1,7 +1,7 @@
 import { migrationStatus } from "./store"
 import { OLD_ORIGIN } from "./origins"
 import { trainers } from "../trainers"
-import { getReadKeys } from "../data/supabase"
+import { TrainerLocalStorage } from "../data/TrainerLocalStorage"
 import * as Analytics from "$lib/site/analytics"
 
 type MigrationMessageData = "failed" | "manual" | [string, string][]
@@ -14,7 +14,7 @@ export function respondToMessage(event: MessageEvent<MigrationMessageData>) {
 		Analytics.createTrainerRecoveryEvent("failed")
 	} else {
 		console.log("Migrating:", event.data)
-		const existingKeys = getReadKeys()
+		const existingKeys = TrainerLocalStorage.getReadKeys()
 		const migratedKeys = event.data.find((it) => it[0] === "trainers")?.[1]?.split(",")?.filter((it) => it !== "") ?? []
 		const combinedKeys = combineListsNoRepeats(existingKeys, migratedKeys)
 		localStorage.setItem("trainers", combinedKeys.join(","))
@@ -25,7 +25,7 @@ export function respondToMessage(event: MessageEvent<MigrationMessageData>) {
 
 		// The store's pretty complicated, but essentially to force trainers into state,
 		// we have to explicitly subscribe to them, hence the subscribe/unsubscribe in the code
-		Promise.all(getReadKeys().map((key) => trainers.get(key))).then((trainers) => {
+		Promise.all(TrainerLocalStorage.getReadKeys().map((key) => trainers.get(key))).then((trainers) => {
 			trainers.forEach((it) => {
 				const unsubscribe = it.subscribe(() => {})
 				unsubscribe()
