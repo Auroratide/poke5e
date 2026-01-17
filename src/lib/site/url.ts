e2eac5757222ff4dc7116923a76cfbcf6e40198f
Auroratide <tf.auroratide@gmail.com>
2026-01-17 12:25:31 -0600

    (#185) Localize urls

--- src/lib/site/url.ts
+++ src/lib/site/url.ts
@@ -1,58 +1,59 @@
 import { resolve } from "$app/paths"
+import { localizeUrl } from "$lib/site/i18n"
 
 export const Url = {
-	home: () => resolve("/"),
+	home: () => localizeUrl(resolve("/")),
 	fakemon: (key?: string, action?: string) => {
 		const params = new URLSearchParams()
 		if (key) params.append("id", key)
 		if (action) params.append("action", action)
 
-		return resolve("/fakemon") + `?${params.toString()}`
+		return localizeUrl(resolve("/fakemon") + `?${params.toString()}`)
 	},
-	items: (id?: string) => id ? resolve("/items/[id]", { id }) : resolve("/items"),
-	pokemon: (id?: string) => id ? resolve("/pokemon/[id]", { id }) : resolve("/pokemon"),
-	moves: (id?: string) => id ? resolve("/moves/[id]", { id }) : resolve("/moves"),
-	tms: (id?: string) => id ? resolve("/tms/[id]", { id }) : resolve("/tms"),
+	items: (id?: string) => localizeUrl(id ? resolve("/items/[id]", { id }) : resolve("/items")),
+	pokemon: (id?: string) => localizeUrl(id ? resolve("/pokemon/[id]", { id }) : resolve("/pokemon")),
+	moves: (id?: string) => localizeUrl(id ? resolve("/moves/[id]", { id }) : resolve("/moves")),
+	tms: (id?: string) => localizeUrl(id ? resolve("/tms/[id]", { id }) : resolve("/tms")),
 	trainers: (trainerKey?: string, pokemonId?: string, action?: string) => {
 		const params = new URLSearchParams()
 		if (trainerKey) params.append("id", trainerKey)
 		if (pokemonId) params.append("pokemon", pokemonId)
 		if (action) params.append("action", action)
 
-		return resolve("/trainers") + `?${params.toString()}`
+		return localizeUrl(resolve("/trainers") + `?${params.toString()}`)
 	},
-	versionHistory: () => resolve("/version-history"),
-	trainerRecovery: () => resolve("/trainer-recovery"),
-	feedback: () => resolve("/feedback"),
-	accessibility: () => resolve("/accessibility"),
-	privacyPolicy: () => resolve("/privacy-policy"),
+	versionHistory: () => localizeUrl(resolve("/version-history")),
+	trainerRecovery: () => localizeUrl(resolve("/trainer-recovery")),
+	feedback: () => localizeUrl(resolve("/feedback")),
+	accessibility: () => localizeUrl(resolve("/accessibility")),
+	privacyPolicy: () => localizeUrl(resolve("/privacy-policy")),
 	reference: {
-		all: () => resolve("/reference"),
-		introduction: () => resolve("/reference/introduction"),
-		abilities: () => resolve("/reference/abilities"),
-		bonds: () => resolve("/reference/bonds"),
-		breeding: () => resolve("/reference/breeding"),
-		catchingPokemon: () => resolve("/reference/catching-pokemon"),
-		combat: () => resolve("/reference/combat"),
-		damageTypes: () => resolve("/reference/damage-types"),
-		encounters: () => resolve("/reference/encounters"),
-		faintingRestingHealing: () => resolve("/reference/fainting-resting-and-healing"),
-		feats: () => resolve("/reference/feats"),
-		legendaryBattles: () => resolve("/reference/legendary-battles"),
-		natures: () => resolve("/reference/natures"),
-		pokemonLeveling: () => resolve("/reference/pokemon-leveling"),
-		shinyPokemon: () => resolve("/reference/shiny-pokemon"),
-		specializations: () => resolve("/reference/specializations"),
-		status: () => resolve("/reference/status-conditions"),
-		terrainEffects: () => resolve("/reference/terrain-effects"),
-		tms: () => resolve("/reference/tms"),
-		trainerClass: () => resolve("/reference/trainer-class"),
-		trainerLeveling: () => resolve("/reference/trainer-leveling"),
-		trainerPaths: () => resolve("/reference/trainer-paths"),
-		transformations: () => resolve("/reference/pokemon-transformations"),
-		weather: () => resolve("/reference/weather"),
+		all: () => localizeUrl(resolve("/reference")),
+		introduction: () => localizeUrl(resolve("/reference/introduction")),
+		abilities: () => localizeUrl(resolve("/reference/abilities")),
+		bonds: () => localizeUrl(resolve("/reference/bonds")),
+		breeding: () => localizeUrl(resolve("/reference/breeding")),
+		catchingPokemon: () => localizeUrl(resolve("/reference/catching-pokemon")),
+		combat: () => localizeUrl(resolve("/reference/combat")),
+		damageTypes: () => localizeUrl(resolve("/reference/damage-types")),
+		encounters: () => localizeUrl(resolve("/reference/encounters")),
+		faintingRestingHealing: () => localizeUrl(resolve("/reference/fainting-resting-and-healing")),
+		feats: () => localizeUrl(resolve("/reference/feats")),
+		legendaryBattles: () => localizeUrl(resolve("/reference/legendary-battles")),
+		natures: () => localizeUrl(resolve("/reference/natures")),
+		pokemonLeveling: () => localizeUrl(resolve("/reference/pokemon-leveling")),
+		shinyPokemon: () => localizeUrl(resolve("/reference/shiny-pokemon")),
+		specializations: () => localizeUrl(resolve("/reference/specializations")),
+		status: () => localizeUrl(resolve("/reference/status-conditions")),
+		terrainEffects: () => localizeUrl(resolve("/reference/terrain-effects")),
+		tms: () => localizeUrl(resolve("/reference/tms")),
+		trainerClass: () => localizeUrl(resolve("/reference/trainer-class")),
+		trainerLeveling: () => localizeUrl(resolve("/reference/trainer-leveling")),
+		trainerPaths: () => localizeUrl(resolve("/reference/trainer-paths")),
+		transformations: () => localizeUrl(resolve("/reference/pokemon-transformations")),
+		weather: () => localizeUrl(resolve("/reference/weather")),
 	},
-	settings: () => resolve("/settings"),
+	settings: () => localizeUrl(resolve("/settings")),
 	external: {
 		auroratide: () => "https://auroratide.com",
 		github: () => "https://github.com/Auroratide/poke5e",
