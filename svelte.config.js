0242bbef52b0a1d5f7c390c18dab98cb6f322cd4
Auroratide <tf.auroratide@gmail.com>
2026-01-30 07:31:23 -0600

    Revert "Proof of concept: Translating reference pages"
    This reverts commit 67cd3d24d2486d4635100a087ef00773627579b9.

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,34 @@
-import { mdsvex } from "mdsvex";
-
 /* eslint-disable no-undef */
-import adapter from "@sveltejs/adapter-static";
-
-import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static"
+import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), mdsvex()],
+	preprocess: preprocess(),
+
 	kit: {
 		prerender: {
 			entries: [
 				"*",
 				// backward compatibility
 				"/pokemon.json",
-				"/pokemon/[id].json"
+				"/pokemon/[id].json",
 			],
-
 			handleHttpError: ({ path, message }) => {
 				// idk why, but it likes to include this bit when scanning
-				if (path.includes("[id]")) return;
-
-				throw new Error(message);
-			}
+				if (path.includes("[id]")) return
+				throw new Error(message)
+			},
 		},
-		paths: { relative: false },
-		adapter: adapter({ fallback: "404.html" })
+		paths: {
+			relative: false,
+		},
+		adapter: adapter({
+			fallback: "404.html",
+		}),
 	},
-	extensions: [".svelte", ".svx"]
-};
+}
 
-export default config;
+export default config
