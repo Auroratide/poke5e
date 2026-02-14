2f9de20b1b84798b8a4536b0408318aa67bfbaa0
Auroratide <tf.auroratide@gmail.com>
2026-02-14 13:28:11 -0600

    (#162) Apply filters to fakemon

--- src/lib/fakemon/page/FakemonList.svelte
+++ src/lib/fakemon/page/FakemonList.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Button } from "$lib/ui/elements"
 	import { SortableTable, BubbleRow, ListHeading } from "$lib/ui/page"
-	import { SearchField } from "$lib/ui/forms"
+	import { RelativeNumberField, SearchField, SelectField, type RelativeValue } from "$lib/ui/forms"
 	import { Url } from "$lib/site/url"
 	import type { Fakemon } from "../Fakemon"
 	import type { FakemonListStore } from "../store"
@@ -9,15 +9,77 @@
 	import GetStarted from "./GetStarted.svelte"
 	import { fakemonListSorter, fakemonListFilter } from "./sort-and-filter"
 	import { m } from "$lib/site/i18n"
+	import { SpeciesFilter } from "$lib/poke5e/species"
+	import { PokemonType, type PokeType } from "$lib/pokemon/types"
+	import { CreatureSizes, type CreatureSize } from "$lib/dnd/CreatureSize"
+	import { capitalize } from "$lib/utils/string"
+	import { EggGroup } from "$lib/pokemon/egg-group"
+	import { BiomesStore } from "$lib/poke5e/habitat"
 
 	export let fakemon: FakemonListStore
 	export let showGetStarted = false
 
 	$: hasNoFakemon = $fakemon.length === 0
-	$: filtered = $fakemon.filter((it) => it.data.species.name.toLocaleLowerCase().includes($fakemonListFilter.toLocaleLowerCase()))
+
+	const AnyOption = [ {
+		name: `- ${m["universal.any"]()} -`,
+		value: "",
+	} ]
+
+	let filteredType: PokeType | "" = ""
+	let typeOptions = AnyOption.concat(Object.values(PokemonType.list).map((it) => ({
+		value: it,
+		name: capitalize(it),
+	})))
+
+	let filteredSize: CreatureSize | "" = ""
+	const sizeOptions = AnyOption.concat(Object.values(CreatureSizes).map((it) => ({
+		value: it,
+		name: capitalize(it),
+	})))
+
+	let filteredSr: number | undefined = undefined
+	let filteredSrRelative: RelativeValue = "="
+
+	let filteredMinLevel: number | undefined = undefined
+	let filteredMinLevelRelative: RelativeValue = "="
+
+	let filteredEggGroup: string = ""
+	let eggGroupOptions = AnyOption.concat(EggGroup.standardList.map((it) => ({
+		value: it,
+		name: capitalize(it),
+	})))
+
+	let filteredBiome: string = ""
+	$: biomeOptions = AnyOption.concat($BiomesStore?.map((it) => ({
+		value: it.id,
+		name: it.name,
+	})) ?? [])
+
+	$: textFilterIsPokemonType = PokemonType.list.includes($fakemonListFilter.toLocaleLowerCase() as PokeType)
+
+	$: filter = new SpeciesFilter()
+		.name(textFilterIsPokemonType ? "" : $fakemonListFilter)
+		.type(filteredType !== "" ? filteredType : textFilterIsPokemonType ? $fakemonListFilter : "")
+		.size(filteredSize)
+		.sr(filteredSrRelative, filteredSr)
+		.minLevel(filteredMinLevelRelative, filteredMinLevel)
+		.eggGroup(filteredEggGroup)
+		.biome(filteredBiome)
+
+	$: filtered = $fakemon.filter((it) => filter.apply(it.species))
 
 	const byStringField = (field: (m: Fakemon) => string) =>
 		(l: Fakemon, r: Fakemon) => field(l).localeCompare(field(r))
+	
+	const resetFilters = () => {
+		filteredType = ""
+		filteredSize = ""
+		filteredSr = undefined
+		filteredMinLevel = undefined
+		filteredEggGroup = ""
+		filteredBiome = ""
+	}
 </script>
 
 <ListHeading title="Fakémon" target="/fakemon">
@@ -25,7 +87,14 @@
 	<Button slot="action" href="{Url.fakemon(undefined, PageAction.add)}">+ {m["universal.newFakemon"]()}</Button>
 </ListHeading>
 <div class="space-bottom">
-	<SearchField id="filter-fakemon" label="Search" bind:value={$fakemonListFilter} matched={filtered.length} max={$fakemon.length} />
+	<SearchField id="filter-fakemon" label="Search" bind:value={$fakemonListFilter} matched={filtered.length} max={$fakemon.length} activeFilters={filter.count() - ($fakemonListFilter !== "" ? 1 : 0)} on:reset={resetFilters}>
+		<SelectField label="{m["universal.type"]()}" bind:value={filteredType} options={typeOptions} />
+		<SelectField label="{m["universal.size"]()}" bind:value={filteredSize} options={sizeOptions} />
+		<RelativeNumberField label="{m["universal.sr"]()}" bind:value={filteredSr} bind:relative={filteredSrRelative} min={0} max={15} placeholder="{m["universal.number"]()}" />
+		<RelativeNumberField label="{m["universal.minLevel"]()}" bind:value={filteredMinLevel} bind:relative={filteredMinLevelRelative} min={0} max={20} placeholder="{m["universal.number"]()}" />
+		<SelectField label="{m["universal.eggGroup"]()}" bind:value={filteredEggGroup} options={eggGroupOptions} />
+		<SelectField label="{m["universal.biome"]()}" bind:value={filteredBiome} options={biomeOptions} />
+	</SearchField>
 </div>
 {#if hasNoFakemon}
 	{#if showGetStarted}<GetStarted />{/if}
