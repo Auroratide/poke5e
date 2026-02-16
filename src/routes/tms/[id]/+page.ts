3e3cd1af2f241c5d46177f048e2fd6ffd59c6f03
Auroratide <tf.auroratide@gmail.com>
2026-02-16 16:53:23 -0600

    Apply spanish translations to tms

--- src/routes/tms/[id]/+page.ts
+++ src/routes/tms/[id]/+page.ts
@@ -1,12 +1,12 @@
 import type { PageLoad } from "./$types"
 import { error } from "@sveltejs/kit"
-import { resolve } from "$app/paths"
 import type { Data } from "$lib/DataClass"
 import { Move } from "$lib/moves/Move"
 import type { Tm } from "$lib/moves/tms/Tm"
+import { Url } from "$lib/site/url"
 
 export const load: PageLoad = async ({ fetch, params }) => {
-	return fetch(resolve("/moves.json")).then(async (res) => {
+	return fetch(Url.api.moves()).then(async (res) => {
 		const data: { moves: Data<Move>[] } = await res.json()
 		const moves = data.moves.map((it) => new Move(it))
 
