817e48e9635ad6fbe3dbc2718c14ac325a71f885
Auroratide <tf.auroratide@gmail.com>
2026-03-21 08:10:55 -0400

    typo

--- src/routes/settings/RulesVersionSection.svelte
+++ src/routes/settings/RulesVersionSection.svelte
@@ -8,7 +8,7 @@
 	<h2>Rules Version</h2>
 	<p>There are two versions of Pokémon 5e:</p>
 	<ul>
-		<li><strong>2018</strong>: The original version maintained in 2018 that was eventually discontnued.</li>
+		<li><strong>2018</strong>: The original version maintained in 2018 that was eventually discontinued.</li>
 		<li><strong>2024</strong>: The current version, updated to follow D&D 2024 and include newer Pokémon content and mechanics.</li>
 	</ul>
 	<p>You may change version using the toggle below. Refer to the <a href="{Url.reference.introduction()}">Reference</a> for more details.</p>
