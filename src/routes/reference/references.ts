539d5175052e9386e11fe8e652e2163fd2a25403
Auroratide <tf.auroratide@gmail.com>
2026-03-29 22:56:06 -0400

    Hide trainer origins for now, would like to add paldean first

--- src/routes/reference/references.ts
+++ src/routes/reference/references.ts
@@ -95,10 +95,10 @@ export const References = [ {
 	url: Url.reference.trainerLeveling(),
 	keywords: ["class", "trainer", "table", "experience", "milestones", "pokedex", "registered", "pokeslots", "levels"],
 }, {
-	name: "Trainer Origins",
-	url: Url.reference.trainerOrigins(),
-	keywords: ["origins", "races", "alolan", "galarian", "hoennian", "johtoan", "kalosian", "kantoan", "sinnoan", "unovan", "way of life", "one with nature", "a different bond", "a good old fashioned brawl", "you wot mate", "me mum hits me harder", "seen it all", "well equipped", "no place like home", "steeped in tradition", "well learned", "practiced strikes", "c'est la vie", "silver tongue", "bon chance", "forge your path", "intuitive", "my time to shine", "hardy", "i can get that", "body and mind", "another day in the life", "you can't be serious", "many talents"],
-}, {
+// 	name: "Trainer Origins",
+// 	url: Url.reference.trainerOrigins(),
+// 	keywords: ["origins", "races", "alolan", "galarian", "hoennian", "johtoan", "kalosian", "kantoan", "sinnoan", "unovan", "way of life", "one with nature", "a different bond", "a good old fashioned brawl", "you wot mate", "me mum hits me harder", "seen it all", "well equipped", "no place like home", "steeped in tradition", "well learned", "practiced strikes", "c'est la vie", "silver tongue", "bon chance", "forge your path", "intuitive", "my time to shine", "hardy", "i can get that", "body and mind", "another day in the life", "you can't be serious", "many talents"],
+// }, {
 	name: "Trainer Paths",
 	url: Url.reference.trainerPaths(),
 	keywords: ["ace trainer", "battle master", "rapid switching", "tactical mastery", "hobbyist", "versatile", "generalist", "multitalented", "nurse", "pokechef", "warning words", "tip-top shape", "tip top shape", "researcher", "analyst", "evolution expert", "professor", "ranger", "capture styler", "partners", "poke assist"],
