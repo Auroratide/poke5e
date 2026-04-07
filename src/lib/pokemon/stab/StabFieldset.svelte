57d209cb7fe7817f735cc44f13c335216374ed72
Auroratide <tf.auroratide@gmail.com>
2026-04-07 17:41:12 -0500

    (#76) Official messages for the stab customization

--- src/lib/pokemon/stab/StabFieldset.svelte
+++ src/lib/pokemon/stab/StabFieldset.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Fieldset, InstructionText, IntField, SelectField } from "$lib/ui/forms"
 	import type { Stab, StabBase } from "./Stab"
+	import { m } from "$lib/site/i18n"
 
 	export let value: Stab
 	export let disabled = false
@@ -10,24 +11,36 @@
 		name: string,
 	}[] = [ {
 		value: "default",
-		name: "Default",
+		name: m.default(),
 	}, {
 		value: "movepower",
-		name: "Move Power",
+		name: m.movePower(),
 	}, {
 		value: "proficiency",
-		name: "Proficiency Bonus",
+		name: m.proficiencyBonus(),
 	}, {
 		value: "ruleset18",
-		name: "'18 Ruleset",
+		name: m.ruleset18(),
 	}, {
 		value: "none",
-		name: "None",
+		name: m.none(),
 	} ]
+
+	const baseDescriptions = {
+		default: m.customStabDefaultDescription(),
+		movepower: m.customStabMovePowerDescription(),
+		proficiency: m.customStabProficiencyDescription(),
+		ruleset18: m.customStabRuleset18Description(),
+		none: m.customStabNoneDescription(),
+	}
+
+	$: selectedOption = baseOptions.find((it) => it.value === value.base)
+	$: description = baseDescriptions[selectedOption.value]
 </script>
 
 <Fieldset title="STAB Customization">
-	<InstructionText>Hi this is what this is.</InstructionText>
+	<InstructionText>{m.customStabInstructions()}</InstructionText>
 	<SelectField label="STAB Base" bind:value={value.data.base} options={baseOptions} {disabled} />
+	<InstructionText><strong>{selectedOption.name}:</strong> {description}</InstructionText>
 	<IntField label="STAB Bonus" bind:value={value.data.bonus} {disabled} />
 </Fieldset>
