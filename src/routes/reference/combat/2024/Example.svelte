7a9e2d5cd9d209324e2bc88d7989cf42447ac320
Auroratide <tf.auroratide@gmail.com>
2026-04-20 13:41:06 -0500

    Update the old example of STAB

--- src/routes/reference/combat/2024/Example.svelte
+++ src/routes/reference/combat/2024/Example.svelte
@@ -6,6 +6,6 @@
 <p>Bug Buzz uses either DEX or CHA as its move power. Our Vivillon's DEX of 17 is higher than it's CHA of 10, so we will use DEX. Therefore, we calculate:</p>
 <ul class="smaller">
 	<li><strong>Save DC = 16</strong>: 8 + 3 (DEX mod) + 5 (proficiency)</li>
-	<li><strong>Damage</strong>: 3d10 (level) + 3 (DEX mod) + 3 (STAB)</li>
+	<li><strong>Damage</strong>: 3d10 (level) + 3 (DEX mod) + 5 (STAB)</li>
 </ul>
-<p>Vivillon's STAB is 3 since Bug Buzz is a bug-type move, and Vivillon is a bug-type Pokémon.</p>
+<p>Vivillon's STAB is 5 since Bug Buzz is a bug-type move, and Vivillon is a bug-type Pokémon.</p>
