b66544dc20103f5e91c0020816409e3dafe5bcfc
Auroratide <tf.auroratide@gmail.com>
2026-05-19 11:21:08 -0500

    (#474) Force rules version to be correct just in case it isn't

--- src/lib/site/rules-version/RulesVersion.ts
+++ src/lib/site/rules-version/RulesVersion.ts
@@ -5,7 +5,11 @@ export type RulesVersion = "2018" | "2024"
 
 const RULES_VERSION_STORAGE_KEY = "rules-version"
 
-const getCurrentVersion = (): RulesVersion => browser ? localStorage.getItem(RULES_VERSION_STORAGE_KEY) as RulesVersion : "2024"
+// Just in case this is somehow very wrong, force it to be right
+const getCurrentVersion = (): RulesVersion => {
+	const version = browser ? localStorage.getItem(RULES_VERSION_STORAGE_KEY) as RulesVersion : "2024"
+	return version === "2018" || version === "2024" ? version : "2024"
+}
 
 export const rulesVersion = writable<RulesVersion>(getCurrentVersion() ?? "2024")
 
