1e8894f950b256b8323cc30f6f048b86aefff1ec
Auroratide <tf.auroratide@gmail.com>
2026-07-18 09:29:27 -0500

    (#288) Remove need for SSRing pokemon

--- src/routes/encounter-tool/+page.ts
+++ src/routes/encounter-tool/+page.ts
@@ -1,12 +1,8 @@
-import { get } from "svelte/store"
-import { PokemonSpecies, SpeciesStore } from "$lib/poke5e/species"
-import type { PageLoad } from "./$types"
 import { Url } from "$lib/site/url"
 import { error } from "@sveltejs/kit"
+import type { PageLoad } from "./$types"
 
 export const load: PageLoad = async ({ fetch }) => {
-	const cached = get(SpeciesStore.canonList())
-
 	const biomes = await fetch(Url.api.biomes()).then(async res => {
 		if (res.status === 404)
 			error(404)
@@ -16,17 +12,5 @@ export const load: PageLoad = async ({ fetch }) => {
 			}
 	})
 
-	if (cached != null && cached.length > 0) {
-		return { pokemonList: cached, biomes }
-	}
-
-	const pokemon = await fetch(Url.api.pokemon())
-		.then(res => res.json())
-		.then((data) => Promise.all(data.items.map((it) => 
-			PokemonSpecies.fromJson(it),
-		)))
-		.then((pokemon: PokemonSpecies[]) => pokemon.filter((it) => !it.wasNonCanonNonFakemon()))
-
-
-	return { pokemonList: pokemon, biomes }
+	return { biomes }
 }
