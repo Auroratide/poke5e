e39e581e2eff28fa1d252a38bf40580955d3f4cf
Auroratide <tf.auroratide@gmail.com>
2026-08-18 09:18:14 -0500

    (#607) For sake of completion, add single biome endpoint to srd

--- src/lib/srd/biomes/client.ts
+++ src/lib/srd/biomes/client.ts
@@ -1,7 +1,7 @@
 import { resolve } from "$app/paths"
 import { localizeUrl } from "$lib/site/i18n"
 import type { Edition } from "../editions"
-import type { BiomesListJson } from "./schema"
+import type { BiomeJson, BiomesListJson } from "./schema"
 
 export class BiomesSrdClient {
 	constructor(readonly edition: Edition, private readonly customFetch = fetch) {}
@@ -9,5 +9,10 @@ export class BiomesSrdClient {
 	all(): Promise<BiomesListJson> {
 		return this.customFetch(localizeUrl(resolve("/srd/v1/[edition]/biomes.json", { edition: this.edition })))
 			.then((res) => res.json())
+	}
+
+	one(id: string): Promise<BiomeJson | undefined> {
+		return this.customFetch(localizeUrl(resolve("/srd/v1/[edition]/biomes/[id].json", { edition: this.edition, id })))
+			.then((res) => res.ok ? res.json() : Promise.resolve(undefined))
 	}
 }
--- src/lib/srd/biomes/server.ts
+++ src/lib/srd/biomes/server.ts
@@ -1,16 +1,41 @@
 import { translateData } from "$lib/site/i18n"
-import type { BiomesListJson } from "./schema"
-import en from "../data/2024/biomes/en.json"
+import type { BiomeJson, BiomesListJson } from "./schema"
+import en2024 from "../data/2024/biomes/en.json"
+import en2018 from "../data/2018/biomes/en.json"
+import { chooseEditionData, type Edition } from "../editions"
 
-async function all(): Promise<BiomesListJson> {
-	const values = await translateData(
-		en.values,
+async function all(edition: Edition): Promise<BiomesListJson> {
+	const values2024 = await translateData(
+		en2024.values,
 		async (locale) => (await import(`./data/2024/biomes/${locale}.json`)).values,
 	)
+
+	const values2018 = await translateData(
+		en2018.values,
+		async (locale) => (await import(`./data/2018/biomes/${locale}.json`)).values,
+	)
+
+	const values = chooseEditionData(edition, values2024, {
+		"2018": values2018,
+	})
 
 	return { values }
 }
 
+function ids(): string[] {
+	return en2024.values.map((it) => it.id)
+}
+
+async function one(id: string, edition: Edition): Promise<BiomeJson | undefined> {
+	const values = await all(edition)
+
+	const single = values.values.find((it) => it.id === id)
+
+	return single
+}
+
 export const BiomesSrd = {
 	all,
+	ids,
+	one,
 } as const
