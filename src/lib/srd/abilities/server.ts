bd3bc0c6523e378aebd26678ec0fca8fcfc68a95
Auroratide <tf.auroratide@gmail.com>
2026-08-26 11:40:14 -0500

    (#607) Add the translated abilities to the new SRD system

--- src/lib/srd/abilities/server.ts
+++ src/lib/srd/abilities/server.ts
@@ -7,15 +7,15 @@ import en2018 from "../data/2018/abilities/en.json"
 async function all(edition: Edition): Promise<AbilityListJson> {
 	const values2024 = await translateData(
 		en2024.values,
-		async (locale) => (await import(`./data/2024/abilities/${locale}.json`)).values,
+		async (locale) => (await import(`../data/2024/abilities/${locale}.json`)).values,
 	)
 
 	const values2018 = await translateData(
 		en2018.values,
-		async (locale) => (await import(`./data/2018/abilities/${locale}.json`)).values,
+		async (locale) => (await import(`../data/2018/abilities/${locale}.json`)).values,
 	)
 
-	const values = chooseEditionData(edition, values2024, {
+	const values = chooseEditionData<AbilityJson>(edition, values2024, {
 		"2018": values2018,
 	})
 
