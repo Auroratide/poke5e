1d077a6709039b4a616efceada08acd0a16e9bd5
Auroratide <tf.auroratide@gmail.com>
2026-08-31 08:40:51 -0500

    (#657) Function to get random nature

--- src/lib/pokemon/nature/Nature.ts
+++ src/lib/pokemon/nature/Nature.ts
@@ -6,4 +6,10 @@ export class Nature extends DataClass<string> {
 	isStandard(): boolean {
 		return (get(StandardNatures) as string[]).includes(this.data)
 	}
+
+	static random(): Nature {
+		const natures = get(StandardNatures)
+
+		return new Nature(natures[Math.floor(Math.random() * natures.length)])
+	}
 }
